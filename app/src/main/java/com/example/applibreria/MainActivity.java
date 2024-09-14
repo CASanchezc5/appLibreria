b16fc9d8045293ccb05bee360637f74b23889f4b
CASanchezc5 <cascqf@gmail.com>
2024-09-14 08:29:04 -0500

    se agregaron variables globales y creacion del spinner en el MainActivity.java

--- app/src/main/java/com/example/applibreria/MainActivity.java
+++ app/src/main/java/com/example/applibreria/MainActivity.java
@@ -1,6 +1,11 @@
  package com.example.applibreria;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    // Instanciar elementos del archivo xml
+    EditText codeBook, nameBook, costeBook;
+    TextView message;
+    Spinner availableBook;
+    ImageButton save, search;
+
+    // Array que llenara el spinner
+    String[] arrAvailableBook = {"Disponible", "No Disponible"};
+
+    // Instanciar la clase de sqlite
+
+
+    // Generar el objeto de Product
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +38,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Referenciar los objetos con los ids del archivo
+        codeBook = findViewById(R.id.etCodBook);
+        nameBook = findViewById(R.id.etNameBook);
+        costeBook = findViewById(R.id.etCosteBook);
+        message = findViewById(R.id.tvMessage);
+        availableBook = findViewById(R.id.spAvailableBook);
+        save = findViewById(R.id.ibSave);
+        search = findViewById(R.id.ibSearch);
+
+        // Generar el arrarAdapter que será llenado con el arrAvailableBook (spinner)
+        ArrayAdapter<String> adpAvailableBook = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked, arrAvailableBook);
+        // Asignar el anterior adaptador al spinner
+        availableBook.setAdapter(adpAvailableBook);
+
+        // Eventos de cada botón
     }
 }
