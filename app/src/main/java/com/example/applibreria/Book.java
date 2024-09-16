181cd8fab5a58b8fc132ca5cf50089e16690055d
CASanchezc5 <cascqf@gmail.com>
2024-09-15 19:35:21 -0500

    se modifico el archivo Book.java

--- app/src/main/java/com/example/applibreria/Book.java
+++ app/src/main/java/com/example/applibreria/Book.java
@@ -1,4 +1,40 @@
 package com.example.applibreria;
 
 public class Book {
+    private String codeBook;
+    private String nameBook;
+    private int costeBook;
+    private int availableBook;
+
+    public String getCodeBook() {
+        return codeBook;
+    }
+
+    public void setCodeBook(String codeBook) {
+        this.codeBook = codeBook;
+    }
+
+    public String getNameBook() {
+        return nameBook;
+    }
+
+    public void setNameBook(String nameBook) {
+        this.nameBook = nameBook;
+    }
+
+    public int getCosteBook() {
+        return costeBook;
+    }
+
+    public void setCosteBook(int costeBook) {
+        this.costeBook = costeBook;
+    }
+
+    public int getAvailableBook() {
+        return availableBook;
+    }
+
+    public void setAvailableBook(int availableBook) {
+        this.availableBook = availableBook;
+    }
 }
