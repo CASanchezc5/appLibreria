1dc422067f4faf193e5a1845afc923c938b747c6
CASanchezc5 <cascqf@gmail.com>
2024-09-15 19:28:57 -0500

    se modifico archivo DBLibrary.java, se creo la BBDD

--- app/src/main/java/com/example/applibreria/DBLibrary.java
+++ app/src/main/java/com/example/applibreria/DBLibrary.java
@@ -1,4 +1,30 @@
 package com.example.applibreria;
 
-public class DBLibrary {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBLibrary extends SQLiteOpenHelper {
+
+    // Definir la (s) variable (s) para la creación de las tablas
+    String tblBook = "create table book(codeBook text, nameBook, costeBook integer, availableBook integer)";
+
+    public DBLibrary(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Crear las tablas
+        db.execSQL(tblBook);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("drop table book");
+        db.execSQL(tblBook);
+    }
 }
